56738218b9051614c88cf570c3a0f76b48c1aa8c
Rida Faraz <ridafaraz122@gmail.com>
2025-10-21 17:37:39 -0700

    both blinks

--- test_suite.py
+++ test_suite.py
@@ -3,6 +3,7 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
+# Blinking function
 def blink_led(led_gpio=17, blinks=5, interval=0.5):
     for b in range(blinks):
         print(f"Blink {b + 1}")
@@ -15,7 +16,11 @@ def blink_led(led_gpio=17, blinks=5, interval=0.5):
 def main():
     led_gpio = 17
     GPIO.setup(led_gpio, GPIO.OUT)
+    # First blink
     blink_led()
+
+    # Second blink 
+    blink_led(blinks=4, interval=0.2)
     GPIO.cleanup()
 
 if __name__ == '__main__':
