25173998d573bb35c75c49316327af267b86e5f6
chompers98 <leyaageo@usc.edu>
2025-10-21 17:39:20 -0700

    added comments

--- test_suite.py
+++ test_suite.py
@@ -1,9 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
+# set mode as GPIO numbers instead of physical pin numbers
 GPIO.setmode(GPIO.BCM)
 
-# Blinking function
+# blinking function
+    # interval is in seconds 
 def blink_led(led_gpio=17, blinks=5, interval=0.5):
     for b in range(blinks):
         print(f"Blink {b + 1}")
@@ -14,13 +16,17 @@ def blink_led(led_gpio=17, blinks=5, interval=0.5):
 
 
 def main():
+    # led setup
     led_gpio = 17
     GPIO.setup(led_gpio, GPIO.OUT)
-    # First blink
+
+    # first blink
     blink_led()
 
-    # Second blink 
+    # second blink 
     blink_led(blinks=4, interval=0.2)
+
+    # cleanup pin assignments
     GPIO.cleanup()
 
 if __name__ == '__main__':
